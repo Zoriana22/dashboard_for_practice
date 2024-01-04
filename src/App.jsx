e36c42048350084592991e5e2652a541349599ce
PaninaAY <PaninaAY@bk.ru>
2024-01-04 13:25:14 +0200

    +router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import styles from "./App.module.css";
 import Admin_Sidebar from "./Сomponents/Admin_Sidebar/Admin_Sidebar";
 import Admin_wrapper_content from "./Сomponents/Admin_wrapper_content/Admin_wrapper_content";
-//className={styles.test}
 import Topbar from "./Сomponents/Topbar/Topbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><Admin_wrapper_content /></div>,
+  },
+]);
 
 const App = () => {
 
@@ -20,7 +25,8 @@ const App = () => {
       </div>
 
       <div className={styles.app_wrapper_content}>
-        <Admin_wrapper_content />
+        <RouterProvider router={router} />
+        {/*<Admin_wrapper_content />*/}
       </div>
 
     </div>
