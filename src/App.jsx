11af1cbe1990347726cffb8ffef9b96e97214573
PaninaAY <PaninaAY@bk.ru>
2024-01-04 20:38:35 +0200

    routing refactored !

--- src/App.jsx
+++ src/App.jsx
@@ -10,58 +10,24 @@ function App() {
   let element = useRoutes([
     {
       path: "/",
-      element: <div><MainPage /></div>,
+      element: <div><Dashboard /></div>,
     },
     {
       path: "users",
-      element: <div><SecondPage /></div>,
+      element: <div><Users /></div>,
     },
   ]);
-  return element;
+
+  return (
+    <div className={styles.app_wrapper}><Admin_Sidebar />
+
+      <div className={styles.topbar}><Topbar /></div>
+      <div className={styles.app_wrapper_content}> {element} </div>
+
+    </div>
+  );
 }
 
 export default App;
 
-const MainPage = () => {
-  return (
-    <div className={styles.app_wrapper}><Admin_Sidebar />
 
-      <div className={styles.topbar}><Topbar /></div>
-      <div className={styles.app_wrapper_content}><Dashboard /></div>
-
-    </div>
-  );
-}
-
-const SecondPage = () => {
-  return (
-    <div className={styles.app_wrapper}><Admin_Sidebar />
-
-      <div className={styles.topbar}><Topbar /></div>
-      <div className={styles.app_wrapper_content}><Users /></div>
-
-    </div>
-  );
-}
-
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <div><Dashboard /></div>,
-//   },
-//   {
-//     path: "/users",
-//     element: <div><Users /></div>,
-//   }
-// ]);
-
-// children: [
-//   {
-//     path: "/",
-//     element: <div className={styles.topbar}><Topbar /></div>,
-//   },
-//   { path: "/",
-//   element: <div className={styles.app_wrapper_content}><Dashboard /></div> },
-// ],
