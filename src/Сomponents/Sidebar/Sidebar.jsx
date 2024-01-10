30970cd3fc3ce19332f04b108c9cb03844e5f85d
PaninaAY <PaninaAY@bk.ru>
2024-01-10 16:26:19 +0200

    +prettier formating

--- src/Сomponents/Sidebar/Sidebar.jsx
+++ src/Сomponents/Sidebar/Sidebar.jsx
@@ -1,52 +1,57 @@
 //node_modules
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 //components
-import Dashboard from '../Dashboard/Dashboard';
+import Dashboard from "../Dashboard/Dashboard";
 //styles
 import styles from "./Sidebar.module.css";
 
 const Sidebar = (props) => {
-    return (
-        <div className={styles.admin_navbar_container}>
-            <div className={styles.header_title}>
-                <h2 className={styles.header_title_nik}>ADMIN</h2>
-                <div className={styles.burger_menu_btn}>
-                    <div className={styles.burger_line}></div>
-                    <div className={styles.burger_line}></div>
-                    <div className={styles.burger_line}></div>
-                </div>
-            </div>
+  return (
+    <div className={styles.admin_navbar_container}>
+      <div className={styles.header_title}>
+        <h2 className={styles.header_title_nik}>ADMIN</h2>
+        <div className={styles.burger_menu_btn}>
+          <div className={styles.burger_line}></div>
+          <div className={styles.burger_line}></div>
+          <div className={styles.burger_line}></div>
+        </div>
+      </div>
 
-            <div className={styles.admin_ava_pict}>
-                <img src="" alt="admin_ava" />
-                {/* <img src={adminAva} alt="admin_ava" /> */}
-                <h3 className={styles.admin_name}>Jessica Smith </h3>
-                <span className={styles.admin_job_descr}>Smart Admin</span>
-            </div>
+      <div className={styles.admin_ava_pict}>
+        <img src="" alt="admin_ava" />
+        {/* <img src={adminAva} alt="admin_ava" /> */}
+        <h3 className={styles.admin_name}>Jessica Smith </h3>
+        <span className={styles.admin_job_descr}>Smart Admin</span>
+      </div>
 
-            <div className={styles.dashboard_box}>
-                <img src="" alt=" " />
-                <span className={styles.dashboard_title}>< Dashboard /></span>
-            </div>
+      <div className={styles.dashboard_box}>
+        <img src="" alt=" " />
+        <span className={styles.dashboard_title}>
+          <Dashboard />
+        </span>
+      </div>
 
-            <><h3 className={styles.admin_data_title}>Data</h3></>
-            <ul>
-                <li className={styles.data_item}>
-                    <img src="" alt=" " />
-                    <Link to={`/users`}><span className={styles.admin_data_title}>Users </span></Link>
-                </li>
-                <li className={styles.data_item}>
-                    <img src="" alt=" " />
-                    <span className={styles.data_item_title}>Contacts Information</span>
-                </li>
-                <li className={styles.data_item}>
-                    <img src="" alt=" " />
-                    <span className={styles.data_item_title}>Invoices Balances</span>
-                </li>
-            </ul>
-
-        </div >
-    )
-}
+      <>
+        <h3 className={styles.admin_data_title}>Data</h3>
+      </>
+      <ul>
+        <li className={styles.data_item}>
+          <img src="" alt=" " />
+          <Link to={`/users`}>
+            <span className={styles.admin_data_title}>Users </span>
+          </Link>
+        </li>
+        <li className={styles.data_item}>
+          <img src="" alt=" " />
+          <span className={styles.data_item_title}>Contacts Information</span>
+        </li>
+        <li className={styles.data_item}>
+          <img src="" alt=" " />
+          <span className={styles.data_item_title}>Invoices Balances</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 export default Sidebar;
--- src/Сomponents/Topbar/Topbar.jsx
+++ src/Сomponents/Topbar/Topbar.jsx
@@ -1,14 +1,10 @@
 //node_modules
-import React from 'react';
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 //styles
 import styles from "./Topbar.module.css";
 
 const Topbar = (props) => {
-    return (
-        <div className={styles.topbar}>
-            I am Supertopbar
-        </div>
-    )
-}
+  return <div className={styles.topbar}>I am Supertopbar</div>;
+};
 export default Topbar;
