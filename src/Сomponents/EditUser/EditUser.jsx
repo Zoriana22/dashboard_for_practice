eb7d73ad2c15c29d0b8619414eb596463228997a
PaninaAY <PaninaAY@bk.ru>
2024-01-15 10:13:09 +0200

    +-format

--- src/Сomponents/EditUser/EditUser.jsx
+++ src/Сomponents/EditUser/EditUser.jsx
@@ -9,9 +9,9 @@ import styles from "./EditUser.module.css";
 
 const EditUser = (props) => {
   const [name, surname, role, phoneNumber, email] = useState("");
-  
+
   const getUserValue = useSelector((state) => state.users.id);
-  
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getOneUser());
@@ -22,46 +22,79 @@ const EditUser = (props) => {
       <h2>Edit Users</h2>
 
       <div className={styles.edit_inputs}>
-        {getUserValue&&getUserValue.map((userValue) => {
-          return (
-        
-        <><label className={styles.edit_label_item}>
-              <input
-                name="name"
-                placeholder="name"
-                className={styles.edit_input_item}>{userValue.name}
-              </input>
+        {getUserValue &&
+          getUserValue.map((userValue) => {
+            return (
+              <>
+                <label className={styles.edit_label_item}>
+                  <input
+                    name="name"
+                    placeholder="name"
+                    className={styles.edit_input_item}
+                  >
+                    {userValue.name}
+                  </input>
+                </label>
 
-            </label><label className={styles.edit_label_item}>
-                <input
-                  name="surname"
-                  placeholder="surname"
-                  className={styles.edit_input_item}>{userValue.surname}
-                </input>
+                <label className={styles.edit_label_item}>
+                  <input
+                    name="surname"
+                    placeholder="surname"
+                    className={styles.edit_input_item}
+                  >
+                    {userValue.surname}
+                  </input>
+                </label>
 
-              </label><label className={styles.edit_label_item}>
-                <select name="selectrole" defaultValue="some role" className={styles.edit_select_item}>
-                  <option className={styles.edit_option_item} value="someOption">some role</option>
-                  <option className={styles.edit_option_item} value="someOption">other option</option>
-                  <option className={styles.edit_option_item} value="otherOption">other option</option>
-                </select>
-              </label><label className={styles.edit_label_item}>
-                <input
-                  name="phoneNumber"
-                  placeholder="phoneNumber"
-                  className={styles.edit_input_item}>{userValue.phone}
-                </input>
+                <label className={styles.edit_label_item}>
+                  <select
+                    name="selectrole"
+                    defaultValue="some role"
+                    className={styles.edit_select_item}
+                  >
+                    <option
+                      className={styles.edit_option_item}
+                      value="someOption"
+                    >
+                      some role
+                    </option>
+                    <option
+                      className={styles.edit_option_item}
+                      value="someOption"
+                    >
+                      other option
+                    </option>
+                    <option
+                      className={styles.edit_option_item}
+                      value="otherOption"
+                    >
+                      other option
+                    </option>
+                  </select>
+                </label>
 
-              </label><label className={styles.edit_label_item}>
-                <input
-                  name="email"
-                  placeholder="email"
-                  className={styles.edit_input_item}>{userValue.email}
-                </input>
+                <label className={styles.edit_label_item}>
+                  <input
+                    name="phoneNumber"
+                    placeholder="phoneNumber"
+                    className={styles.edit_input_item}
+                  >
+                    {userValue.phone}
+                  </input>
+                </label>
 
-              </label></>
-          );
-        })}
+                <label className={styles.edit_label_item}>
+                  <input
+                    name="email"
+                    placeholder="email"
+                    className={styles.edit_input_item}
+                  >
+                    {userValue.email}
+                  </input>
+                </label>
+              </>
+            );
+          })}
       </div>
     </>
   );
